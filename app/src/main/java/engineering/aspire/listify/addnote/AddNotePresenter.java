001142f9aef93b1d3ed0485cd79f09b3473e1b52
Thomas Phillips <thomasphillips3@gmail.com>
2016-10-21 23:36:49 -0400

    AddNotePresenter.java

---
+++ app/src/main/java/engineering/aspire/listify/addnote/AddNotePresenter.java
@@ -0,0 +1,80 @@
+package engineering.aspire.listify.addnote;
+
+import android.support.annotation.NonNull;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import engineering.aspire.listify.data.NotesRepository;
+import engineering.aspire.listify.util.ImageFile;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Created by thomas on 10/20/16.
+ */
+
+public class AddNotePresenter implements AddNoteContract.UserActionsListener {
+
+    @NonNullclear
+
+    private final NotesRepository mNotesRepository;
+    @NonNull
+    private final AddNoteContract.View mAddNoteView;
+    @NonNull
+    private final ImageFile mImageFile;
+
+    public AddNotePresenter(@NonNull NotesRepository notesRepository,
+                            @NonNull AddNoteContract.View addNoteView,
+                            @NonNull ImageFile imageFile) {
+        mNotesRepository = checkNotNull(notesRepository);
+        mAddNoteView = checkNotNull(addNoteView);
+        mImageFile = imageFile;
+    }
+
+    @Override
+    public void saveNote(String title, String description) {
+        String imageUrl = null;
+        if (mImageFile.exists()) {
+            imageUrl = mImageFile.getPath();
+        }
+        Note newNote = new Note(title, description, imageUrl);
+        if (newNote.isEmpty()) {
+            mAddNoteView.showEmptyNoteError();
+        } else {
+            mNotesRepository.saveNote(newNote);
+            mAddNoteView.showNotesList();
+        }
+    }
+
+    @Override
+    public void takePicture() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        mImageFile.create(imageFileName, ".jpg");
+        mAddNoteView.openCamera(mImageFile.getPath());
+    }
+
+    @Override
+    public void imageAvailable() {
+        if (mImageFile.exists()) {
+            mAddNoteView.showImagePreview(mImageFile.getPath());
+        } else {
+            imageCaptureFailed();
+        }
+    }
+
+    @Override
+    public void imageCaptureFailed() {
+        captureFailed();
+    }
+
+    private void captureFailed
+
+    {
+        mImageFile.delete();
+        mAddNoteView.showImageError();
+    }
+}
+
